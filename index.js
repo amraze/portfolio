0650b1cd1057e1926ab8297f59e9e539aac84cf2
Ahmed Mestiri <ahmed.mestiri@supcom.tn>
2023-01-05 00:34:11 +0100

    add aos

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ var bubblyButtons = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
 }
+
+const aosAnimation = document.querySelectorAll('[data-aos]');
+observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('aos-animate');
+        } else {
+            entry.target.classList.remove('aos-animate');
+        }
+    });
+});
+aosAnimation.forEach(aosObject => {
+    observer.observe(aosObject);
+});
+
+
+// AOS
+AOS.init();
